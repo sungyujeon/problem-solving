5c6c56461beb3a09c8d4cd862f307569f3bb66dc
sungyujeon <sungyujeon91@gmail.com>
2021-11-01 23:07:20 +0900

    211101

---
+++ Leetcode/27.js
@@ -0,0 +1,16 @@
+// Leetcode no.27 easy
+// Remove Element
+
+const removeElement = (nums, val) => {
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === val) {
+      nums.splice(i, 1)
+      i--
+    }
+  }
+  return nums.length
+}
+
+const nums = [3,2,2,3]
+const val = 3
+console.log(removeElement(nums, val))
